644531e42bba0fbeef761c4189e3a42dab87c43d
niallwhocodes <n.d12@hotmail.co.uk>
2023-05-26 23:24:14 +0100

    updated bug in JS for smaller screens

--- scripts/app.js
+++ scripts/app.js
@@ -11,11 +11,11 @@ const observer = new IntersectionObserver((entries) => {
         
     })
 },
-{ threshold: 0.5
+{ threshold: [0.5, 0]
 });
 //
 for (let i = 0; i < the_animation.length; i++) {
 const elements = the_animation[i];
 
 observer.observe(elements);
-} 
+}
